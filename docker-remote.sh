4f459754773228cb8a7419dbda57de6c4b300a2c
a-cinchetti <andrea.cinchetti@gmail.com>
2022-08-28 17:15:26 +0200

    fix last commit

--- docker-remote.sh
+++ docker-remote.sh
@@ -1,4 +1,4 @@
 # only with gcloud prompt
 # IMPORTANT change the tag of the img!!!
  docker build -t eu.gcr.io/fiery-airlock-360114/cmservice-client:2.0 .
- docker push -t eu.gcr.io/fiery-airlock-360114/cmservice-client:2.0
+ docker push eu.gcr.io/fiery-airlock-360114/cmservice-client:2.0
