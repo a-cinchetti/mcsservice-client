dddf8183cb8b3782b60ffd9c416e28aa519932d3
a-cinchetti <andrea.cinchetti@gmail.com>
2022-10-16 13:13:26 +0200

    add contacts page

--- src/app/pages/contacts/contacts.module.ts
+++ src/app/pages/contacts/contacts.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactsComponent } from './contacts.component';
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
 
 
 
 @NgModule({
   declarations: [
-    ContactsComponent
+    ContactsComponent,
+    ContactsListComponent
   ],
   imports: [
     CommonModule
