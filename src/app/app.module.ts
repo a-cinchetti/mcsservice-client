6952c351d11a874decae3211c863d7008c6f80cf
a-cinchetti <andrea.cinchetti@gmail.com>
2022-11-02 10:20:33 +0100

    fix graphics mobile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
 import {InlineSVGDirective, InlineSVGModule} from 'ng-inline-svg-2';
 import {HttpClientModule} from "@angular/common/http";
+import {MatButtonModule} from "@angular/material/button";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
@@ -21,7 +24,10 @@ import {HttpClientModule} from "@angular/common/http";
     MatToolbarModule,
     AppRoutingModule,
     InlineSVGModule.forRoot(),
-    HttpClientModule
+    HttpClientModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent],
