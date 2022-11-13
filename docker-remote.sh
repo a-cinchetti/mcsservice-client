5bc7c78cefa0440d7d90be71ab5400dbdc08b8d9
a-cinchetti <andrea.cinchetti@gmail.com>
2022-11-13 15:33:25 +0100

    add new api key

--- docker-remote.sh
+++ docker-remote.sh
@@ -1,4 +1,4 @@
 # only with gcloud prompt
 # IMPORTANT change the tag of the img!!!
- docker build -t eu.gcr.io/fiery-airlock-360114/cmservice-client:2.0 .
- docker push eu.gcr.io/fiery-airlock-360114/cmservice-client:2.0
+ docker build -t eu.gcr.io/high-gecko-368118/cmservice-client:2.0 .
+ docker push eu.gcr.io/high-gecko-368118/cmservice-client:2.0
